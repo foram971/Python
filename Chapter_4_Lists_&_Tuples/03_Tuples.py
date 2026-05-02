0622c7583294091f1abac3c9d163657e319f0b12
foram971 <shahforamnimeshbhai@gmail.com>
2026-05-02 14:46:37 -0400

    Add Tuples file

---
+++ Chapter_4_Lists_&_Tuples/03_Tuples.py
@@ -0,0 +1,7 @@
+#Tuple is an immutable data type in python
+a=(1,45,False,True,"Foram",44.9)
+print(type(a))
+
+#create tuple with only one element
+b=(3,)
+print(type(b))
---
+++ Chapter_4_Lists_&_Tuples/04_Tuple_methods.py
@@ -0,0 +1,8 @@
+a=(12,45,False,True,"Foram",44.9)
+print(a)
+
+b = a.count(45)
+print(b)
+
+i = a.index(True)
+print(i)
