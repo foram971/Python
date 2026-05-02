8cba102df63931a3857e0aea4fd932bfc3729fab
foram971 <shahforamnimeshbhai@gmail.com>
2026-05-02 14:02:18 -0400

    Create 05_Problem5.py

---
+++ Chapter_3_Problem_solving/05_Problem5.py
@@ -0,0 +1,4 @@
+#. Write a program to format the following letter using escape sequence characters.
+#letter = "Dear Foram, this python course is nice. Thanks!"
+letter="Dear Foram,\n\tThis python course is nice.\nThanks!"
+print(letter)
