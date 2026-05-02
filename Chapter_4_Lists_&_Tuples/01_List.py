3c545b3e5c22589878db0e76e50a1275453c0f25
foram971 <shahforamnimeshbhai@gmail.com>
2026-05-02 14:16:11 -0400

    Chapter_4 added

---
+++ Chapter_4_Lists_&_Tuples/01_List.py
@@ -0,0 +1,13 @@
+#Python lists are containers to store a set of values of any data type.
+
+
+friends = ['Foram','Rohan','Apple',10,False]
+print(friends[3])
+
+#List are mutables
+friends[3]='Grapes'
+print(friends[3])
+print(friends)
+
+#slicing
+print(friends[2:4])
---
+++ Chapter_4_Lists_&_Tuples/02_List_methods.py
@@ -0,0 +1,5 @@
+friends = ['Foram','Rohan','Apple',10,False,'Grapes','Berry']
+
+#Append , It add at the end of list 
+friends.append('True')
+print(friends)
