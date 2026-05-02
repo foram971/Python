2296ed33385c9ee87b62f9701ad552b45add7229
foram971 <shahforamnimeshbhai@gmail.com>
2026-05-02 15:13:01 -0400

    Problem solving for chapter 4

---
+++ Chapter_4_Problem_solving/03_Problem3.py
@@ -0,0 +1,6 @@
+#3. Check that a tuple type cannot be changed in python.
+t1=(4,32,55,"Foram")
+
+#t1[1]="ABC" -- you will get error when you run this ,
+# 'tuple' object does not support item assignment
+
---
+++ Chapter_4_Problem_solving/04_Problem4.py
@@ -0,0 +1,3 @@
+#4. Write a program to sum a list with 4 numbers.
+l1=[33,56,88,22]
+print(sum(l1))
