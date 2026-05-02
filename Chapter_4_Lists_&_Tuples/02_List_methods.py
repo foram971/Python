8d68b25de099b52097ed91ab1ab263ca39bf62cc
foram971 <shahforamnimeshbhai@gmail.com>
2026-05-02 14:30:31 -0400

    Create 02_List_methods.py

---
+++ Chapter_4_Lists_&_Tuples/02_List_methods.py
@@ -0,0 +1,21 @@
+friends = ['Foram','Rohan','Apple',10,False,'Grapes','Berry',8]
+
+#Append , It add at the end of list 
+friends.append('True')
+print(friends)
+
+
+n1=[11,22,40,1,7,40,0]
+# updates the list to 
+n1.sort()
+print(n1)
+n1.reverse()# updates the list to [15,21,2,7,8,1]
+print(n1)
+n1.append(8)# adds 8 at the end of the list
+print(n1)
+n1.insert(3,8)# This will add 8 at 3 index
+print(n1)
+n1.pop(1)# Will delete element at index 2 and return its value.
+print(n1)
+n1.remove(40)# Will remove 40 from the list. 
+print(n1)
