175f12efd58a78e69356fcd167f25465a8c275ee
foram971 <shahforamnimeshbhai@gmail.com>
2026-05-02 13:08:49 -0400

    Create 01_Problem1.py

---
+++ Chapter_3_Problem_solving/01_Problem1.py
@@ -0,0 +1,7 @@
+#Write a python program to display a user entered name followed by
+#  Good Afternoon using input () function.
+
+Name = input("Enter Your Name Here:")
+#prefixing a string with f 
+print(f"Good Afternoon,{Name}")
+
