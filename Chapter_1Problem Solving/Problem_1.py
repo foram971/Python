8687dc5a6f18914d3c4a2565d6454458af4b14f7
foram971 <shahforamnimeshbhai@gmail.com>
2026-05-02 13:28:39 -0400

    Update Problem_1.py

--- Chapter_1Problem Solving/Problem_1.py
+++ Chapter_1Problem Solving/Problem_1.py
@@ -1,3 +1,5 @@
+#Write a program to print Twinkle twinkle little star poem in python.
+
 def main():
     poem = "Twinkle, twinkle, little star,\nHow I wonder what you are!\nUp above the world so high,\nLike a diamond in the sky.\nTwinkle, twinkle, little star,\nHow I wonder what you are!"
     print(poem)
